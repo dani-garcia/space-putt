ddcb9469c443f288e0f285935ee55cdee4c94c94
Daniel García <dani-garcia@users.noreply.github.com>
2015-12-06 19:06:13 +0100

    Margenes

--- src/gameScene.js
+++ src/gameScene.js
@@ -2,6 +2,7 @@
 
 var tipoPelota = 1;
 var tipoPlaneta = 2;
+var tipoBorde = 3;
 
 var GameLayer = cc.Layer.extend({
     space: null,
@@ -46,8 +47,25 @@ var GameLayer = cc.Layer.extend({
         var xPelota = this.spritePelota.getBody().p.x;
         var yPelota = this.spritePelota.getBody().p.y;
 
-        this.setPositionX(-xPelota + cc.winSize.width / 2);
-        this.setPositionY(-yPelota + cc.winSize.height / 2);
+        if (cc.winSize.width / 2 > xPelota) {
+            this.setPositionX(0);
+
+        } else if (this.mapa.width - cc.winSize.width / 2 < xPelota) {
+            this.setPositionX(cc.winSize.width - this.mapa.width);
+
+        } else {
+            this.setPositionX(cc.winSize.width / 2 - xPelota);
+        }
+
+        if (cc.winSize.height / 2 > yPelota) {
+            this.setPositionY(0);
+
+        } else if (this.mapa.height - cc.winSize.height / 2 < yPelota) {
+            this.setPositionY(cc.winSize.height - this.mapa.height);
+
+        } else {
+            this.setPositionY(cc.winSize.height / 2 - yPelota);
+        }
 
         this.spritePelota.body.applyForce(this.calcularFuerza(), cp.vzero);
     },
@@ -60,15 +78,15 @@ var GameLayer = cc.Layer.extend({
         // F = --------
         //       d^2
 
-        for (var i = 0; i < this.planetas.length; i++) {
-            var planeta = this.planetas[i];
-            var planetWeight = planeta.diameter / 100;
+        /*for (var i = 0; i < this.planetas.length; i++) {
+         var planeta = this.planetas[i];
+         var planetWeight = planeta.diameter / 100;
 
-            var distXSq = Math.pow(planeta.position.x - posPelota.x, 2);
-            var distYSq = Math.pow(planeta.position.y - posPelota.y, 2);
+         var distXSq = Math.pow(planeta.position.x - posPelota.x, 2);
+         var distYSq = Math.pow(planeta.position.y - posPelota.y, 2);
 
-            fuerza.add(cp.v(planetWeight / distXSq, planetWeight / distYSq));
-        }
+         fuerza.add(cp.v(planetWeight / distXSq, planetWeight / distYSq));
+         }*/
 
         return fuerza;
     },
@@ -90,17 +108,40 @@ var GameLayer = cc.Layer.extend({
         // Ancho del mapa
         this.mapaAncho = this.mapa.getContentSize().width;
 
+        // Pelota
         var pelota = this.mapa.getObjectGroup("Pelota").getObjects();
         cc.assert(pelota.length === 1, "Sólo debe haber una pelota");
 
         this.inicializarPelota(pelota[0].x, pelota[0].y);
 
-
+        // Planetas
         var planetas = this.mapa.getObjectGroup("Planetas").getObjects();
 
         for (var i = 0; i < planetas.length; i++) {
             this.planetas.push(new Planeta(this.space, planetas[i], this));
         }
+
+        // Bordes
+        var bordes = this.mapa.getObjectGroup("Bordes").getObjects();
+        // Los objetos de la capa bordes se transforman a
+        // formas estáticas de Chipmunk ( SegmentShape ).
+        for (var i = 0; i < bordes.length; i++) {
+            var borde = bordes[i];
+            var puntos = borde.polylinePoints;
+            for (var j = 0; j < puntos.length - 1; j++) {
+                var shapeBorde = new cp.SegmentShape(new cp.StaticBody(),
+                    cp.v(parseInt(borde.x) + parseInt(puntos[j].x),
+                        parseInt(borde.y) - parseInt(puntos[j].y)),
+                    cp.v(parseInt(borde.x) + parseInt(puntos[j + 1].x),
+                        parseInt(borde.y) - parseInt(puntos[j + 1].y)),
+                    10);
+
+                shapeBorde.setCollisionType(tipoBorde);
+
+                this.space.addStaticShape(shapeBorde);
+            }
+        }
+
     },
 
     inicializarPelota: function (x, y) {
